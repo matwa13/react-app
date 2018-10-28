c941fbeb0f6baf05c1779b2b675303fbd781141e
matwa13 <matmax@tut.by>
2018-10-28 07:29:42 +0300

    production build

---
+++ build/precache-manifest.7503e381439b9e0e0ea5433357e41d83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa8fd7c82af9b0998079",
+    "url": "/static/js/main.fa8fd7c8.chunk.js"
+  },
+  {
+    "revision": "b60ccf4f55bdf3adb487",
+    "url": "/static/js/1.b60ccf4f.chunk.js"
+  },
+  {
+    "revision": "fa8fd7c82af9b0998079",
+    "url": "/static/css/main.685709aa.chunk.css"
+  },
+  {
+    "revision": "00d6b6ffe66f4cd4b2983eeaf0f68cdc",
+    "url": "/index.html"
+  }
+];
